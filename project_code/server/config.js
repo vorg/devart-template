549b09be68a59a583f7501a18cff9c519d460e42
vorg <marcin.ignac@gmail.com>
2014-03-26 12:27:02 +0000

    Working serverside har generator

---
+++ project_code/server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PHANTOM_PATH: '/Users/vorg/Dev/phantomjs',
+  CACHE_PATH: 'cache'
+}
--- project_code/server/server.js
+++ project_code/server/server.js
@@ -1,14 +1,14 @@
 var exec = require('child_process').exec;
 var fs = require('fs');
+var express = require('express');
+var config = require('./config');
 
-var PHANTOM_PATH = '/Users/vorg/Dev/phantomjs';
-var CACHE_PATH = 'cache';
+var app = express();
 
 function getHar(url, callback) {
   var encodedUrl = new Buffer(url).toString('base64');
-  var harFilePath = CACHE_PATH + '/' + encodedUrl + '.txt';
+  var harFilePath = config.CACHE_PATH + '/' + encodedUrl + '.txt';
   if (fs.existsSync(harFilePath)) {
-    console.log('reading file');
     fs.readFile(harFilePath, { encoding: 'utf-8' }, callback);
   }
   else {
@@ -18,8 +18,8 @@ function getHar(url, callback) {
 
 function generateHar(url, harFilePath, callback) {
   var args = [
-    PHANTOM_PATH + '/bin/phantomjs',
-    PHANTOM_PATH + '/examples/netsniff.js',
+    config.PHANTOM_PATH + '/bin/phantomjs',
+    config.PHANTOM_PATH + '/examples/netsniff.js',
     url,
     '>',
     harFilePath
@@ -42,7 +42,33 @@ function generateHar(url, harFilePath, callback) {
   });
 }
 
-getHar('"http://marcinignac.com"', function(err, har) {
-  if (err) console.log('ERROR', err);
-  else console.log('Done', har);
-})
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://marcinignac.com,http://localhost');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+};
+
+app.configure(function() {
+  app.use(allowCrossDomain);
+});
+
+app.get('/:url', function(req, res){
+  url = req.params.url ? req.params.url : 'http://google.com';
+  console.log('Requesting', url);
+  getHar(req.params.url, function(err, har) {
+    res.setHeader('Content-Type', 'application/json');
+    var json = har;
+    console.log(json);
+    if (err) {
+      console.log('ERROR', err);
+      json = JSON.stringify({ error: err });
+    }
+    res.setHeader('Content-Length', json.length);
+    res.end(json);
+  });
+});
+
+app.listen(1337);
+console.log('Listening on port 1337');
