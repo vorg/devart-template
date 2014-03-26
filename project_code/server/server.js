2b0ada068f88b4de854e079ab60eb26f882b8f52
vorg <marcin.ignac@gmail.com>
2014-03-26 12:05:02 +0000

    Caching har file requests

--- project_code/server/server.js
+++ project_code/server/server.js
@@ -1,24 +1,48 @@
 var exec = require('child_process').exec;
+var fs = require('fs');
 
 var PHANTOM_PATH = '/Users/vorg/Dev/phantomjs';
-var CACHE = 'cache';
+var CACHE_PATH = 'cache';
 
-var url = '"http://marcinignac.com"';
-var encodedUrl = new Buffer(url).toString('base64');
+function getHar(url, callback) {
+  var encodedUrl = new Buffer(url).toString('base64');
+  var harFilePath = CACHE_PATH + '/' + encodedUrl + '.txt';
+  if (fs.existsSync(harFilePath)) {
+    console.log('reading file');
+    fs.readFile(harFilePath, { encoding: 'utf-8' }, callback);
+  }
+  else {
+    generateHar(url, harFilePath, callback);
+  }
+}
 
-console.log(url, encodedUrl)
+function generateHar(url, harFilePath, callback) {
+  var args = [
+    PHANTOM_PATH + '/bin/phantomjs',
+    PHANTOM_PATH + '/examples/netsniff.js',
+    url,
+    '>',
+    harFilePath
+  ];
+  var cmd = args.join(' ');
+  var child = exec(cmd);
+  child.on('close', function(err, msg) {
+    if (err) {
+      callback(err, null);
+    }
+    else {
+      if (fs.existsSync(harFilePath)) {
+        console.log('reading generated file');
+        fs.readFile(harFilePath, { encoding: 'utf-8' }, callback);
+      }
+      else {
+        callback(err, null);
+      }
+    }
+  });
+}
 
-var args = [
-  PHANTOM_PATH + '/bin/phantomjs',
-  PHANTOM_PATH + '/examples/netsniff.js',
-  url,
-  '>',
-  encodedUrl + '.txt'
-];
-
-var cmd = args.join(' ');
-var child = exec(cmd);
-child.on('close', function(err, msg) {
-  console.log('err', err, 'msg', msg);
-  console.log('done')
+getHar('"http://marcinignac.com"', function(err, har) {
+  if (err) console.log('ERROR', err);
+  else console.log('Done', har);
 })
